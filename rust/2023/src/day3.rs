dc820e30332379639848902d88693991d2bde484
Trevor Bedson <trevobedson@gmail.com>
2025-10-28 06:48:27 -0400

    2023 day 3 part 2

--- rust/2023/src/day3.rs
+++ rust/2023/src/day3.rs
@@ -1,8 +1,14 @@
+use std::time::SystemTime;
+
 const INPUT: &str = include_str!("../../../challenge_data/2023/day3.txt");
 
 pub fn day3() {
+    let start = SystemTime::now();
     let p1 = part1();
-    println!("Part 1 ({p1})")
+    let p2 = part2();
+    let duration: u128 = SystemTime::now().duration_since(start).unwrap().as_millis(); 
+
+    println!("Part 1 ({p1}) & Part 2 ({p2}) in {}ms", duration)
 }
 
 fn part1() -> i32 {
@@ -52,6 +58,63 @@ fn part1() -> i32 {
     total
 }
 
-fn part2() {
+fn part2() -> i32 {
+    let grid: Vec<Vec<char>> = INPUT.lines().map(|l| l.chars().collect()).collect();
+    let rows = grid.len();
+    let mut total: i32 = 0;
+    let mut gear_nums: Vec<((usize, usize), i32)> = Vec::new();
 
+    for r in 0..rows {
+        let cols = grid[r].len();
+        let mut c = 0usize;
+        while c < cols {
+            if grid[r][c].is_ascii_digit() {
+                let start = c;
+                let mut num: i32 = 0;
+                while c < cols && grid[r][c].is_ascii_digit() {
+                    num = num * 10 + (grid[r][c] as u8 - b'0') as i32;
+                    c += 1;
+                }
+                let end = c - 1; 
+
+                let r0 = r.saturating_sub(1);
+                let r1 = (r + 1).min(rows - 1);
+                for rr in r0..=r1 {
+                    let row_len = grid[rr].len();
+                    let c0 = start.saturating_sub(1);
+                    let c1 = (end + 1).min(row_len.saturating_sub(1));
+                    for cc in c0..=c1 {
+                        let ch = grid[rr][cc];
+                        if ch == '*' {
+                            gear_nums.push(((rr, cc), num));
+                            break;
+                        }
+                    }
+                }
+            } else {
+                c += 1;
+            }
+        }
+    }
+
+    let mut gear_nums2: Vec<((usize, usize), i32)> = Vec::new();
+
+    for l1 in gear_nums {
+        println!("Gear: (({}, {}), {})", l1.0.0, l1.0.1, l1.1);
+        let mut found = false;
+        for (i, l2) in gear_nums2.iter().enumerate() {
+            if l1.0.0 == l2.0.0 && l1.0.1 == l2.0.1 {
+                total += l1.1 * l2.1;
+                gear_nums2.remove(i);
+                found = true;
+                break
+            }
+        }
+
+        if !found {
+            gear_nums2.push(l1);
+        }
+    };
+
+    total
 }
